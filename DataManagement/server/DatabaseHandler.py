43a3a626d81689da93e2f25e665be3a9ae2c67f5
Gabypic <gpicard77350@gmail.com>
2025-05-12 09:41:15 +0200

    fix thread error by sqlite

--- DataManagement/server/DatabaseHandler.py
+++ DataManagement/server/DatabaseHandler.py
@@ -3,7 +3,8 @@ import sqlite3
 
 class DatabaseHandler():
     def __init__(self, database_name : str):
-        self.connect = sqlite3.connect(f"{os.path.dirname(os.path.abspath(__file__))}/{database_name}")
+        self.connect = sqlite3.connect(f"{os.path.dirname(os.path.abspath(__file__))}/{database_name}",
+                                       check_same_thread=False)
         self.connect.row_factory = sqlite3.Row
 
     def add_data(self, temperature, humidity, gas, steam, date, hour):
